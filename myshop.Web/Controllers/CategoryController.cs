ac7e1acf059b5a23185edaf3b67d7b00d0aa8980
Saifahmeed <saif_ahmed@aucegypt.edu>
2025-06-26 07:09:58 +0300

    Category Controller-Index+Create Actions-

--- myshop.Web/Controllers/CategoryController.cs
+++ myshop.Web/Controllers/CategoryController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using myshop.Web.Data;
+using myshop.Web.Models;
 
 namespace myshop.Web.Controllers
 {
     public class CategoryController : Controller
-    {
+    { 
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
         }
     }
 }
